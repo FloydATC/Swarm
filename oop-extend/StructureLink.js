ebfd1ecc8f6b98584962eac65d7def54b1ff76a1
root <root@localhost.localdomain>
2016-08-04 19:27:38 +0200

    what is going on with link balancing?!

--- oop-extend/StructureLink.js
+++ oop-extend/StructureLink.js
@@ -10,10 +10,12 @@ module.exports = {
     },
 
     execute: function() {
-        console.log(this+' execute()');
+        console.log(this+' execute() with energy='+this.energy+' average='+this.room.link_average);
+
         if (this.cooldown > 0) { console.log(this+' cooling down'); return; } // Unable to transfer
         if (this.energy <= this.room.link_average) { console.log(this+' is below average'); return; } // We should receive, not transmit
         var peer = this.shift_least_energy(this.room.links.slice());
+        console.log('  peer '+peer+' has '+peer.energy+' energy');
         var amount = this.room.link_average - peer.energy;
         console.log(this.room+' '+this+' transferring '+amount+' to '+peer);
         this.transferEnergy(peer, amount);
