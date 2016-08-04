0d610000e9f4eb15dd525b638f940b7d29e73029
root <root@localhost.localdomain>
2016-08-04 18:08:04 +0200

    typo in link execute

--- oop-extend/StructureLink.js
+++ oop-extend/StructureLink.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     shift_least_energy: function(objects) {
-        objects = objects.sort( function(a,b) { a.energy - b.energy; } );
+        objects = objects.sort( function(a,b) { a.energy - b.energy } );
         return objects.shift();
     },
 };
