7a250e45ad8f61531be7e9ffa591f143151e3d34
FloydATC <floyd@atc.no>
2016-08-07 11:01:05 +0200

    added smart harvest flag, the basis for remote mining

--- oop-extend/Flag.js
+++ oop-extend/Flag.js
@@ -6,4 +6,27 @@ module.exports = {
         //console.log(this+' initializing');
     },
 
+    type: function() {
+        // Return name without trailing numbers
+        // E.g. name='harvest2' -> type='harvest'
+        var matches = this.name.match(/^([a-z]+)/);
+        return matches[0];
+    },
+
+    workers: function() {
+        if (typeof this.memory.workers == 'undefined') { this.memory.workers = []; }
+        var creeps = [];
+        for (var i in this.memory.workers) {
+            var id = this.memory.workers[i];
+            var creep = Game.getObjectById(id);
+            if (creep != null) { creeps.push(creep); }
+        }
+        return creeps;
+    },
+
+    assign_worker: function(creep) {
+        if (typeof this.memory.workers == 'undefined') { this.memory.workers = []; }
+        this.memory.workers.push(creep.id);
+        creep.memory.flag = this.id;
+    },
 };
--- oop-extend/Game.js
+++ oop-extend/Game.js
@@ -15,12 +15,28 @@ module.exports = {
             //console.log(this+' flag '+flag+' at '+flag.pos);
 
             // Remote mine sources tagged with a "harvest" flag
-            if (flag.name.startsWith('harvest')) {
-                // TODO ...
+            if (flag.type == 'harvest') {
+                // Find nearest room with an owned controller
+                var lo_range = null;
+                var lo_room = null;
+                for (var name in this.rooms) {
+                    var room = this.rooms[name];
+                    if (room.controller && room.controller.my == true) {
+                        var range = getRoomLinearDistance(this.room.name, room.name);
+                        if (lo_range == null || range < lo_range) {
+                            lo_range = range;
+                            lo_room = room;
+                        }
+                    }
+                }
+                this.memory.owner = lo_room.name;
+                // Add this flag to that room
+                if (!lo_room.harvest_flags) { lo_room.harvest_flags = []; }
+                lo_room.harvest_flags.push(this);
             }
 
             // Colonize rooms with "spawn" flag
-            if (flag.name == 'spawn') {
+            if (flag.type == 'spawn') {
                 var room_name = flag.pos.roomName;
                 if (Game.rooms[room_name] && Game.rooms[room_name].controller && Game.rooms[room_name].controller.my) {
                     var result = Game.rooms[room_name].createConstructionSite(flag.pos, STRUCTURE_SPAWN);
@@ -35,7 +51,7 @@ module.exports = {
             }
 
             // Place tower at "tower" when possible
-            if (flag.name == 'tower') {
+            if (flag.type == 'tower') {
                 var room_name = flag.pos.roomName;
                 if (Game.rooms[room_name] && Game.rooms[room_name].controller && Game.rooms[room_name].controller.my && Game.rooms[room_name].controller.level >= 3) {
                     var result = Game.rooms[room_name].createConstructionSite(flag.pos, STRUCTURE_TOWER);
