1229de2211608e9f75300daaaad0bf3c2ce3e6f4
Floyd <floyd@atc.no>
2016-08-12 15:55:06 +0200

    upgraders need more space

--- oop-extend/Room.js
+++ oop-extend/Room.js
@@ -230,7 +230,7 @@ module.exports = {
             var needs = flag.needs();
             if (needs == 'Zealot') {
                 console.log(this+' spawning a zealot for '+flag.pos.roomName);
-                var result = this.createCreep([WORK,WORK,WORK,WORK,WORK,CARRY,MOVE], undefined, { class: 'Zealot', home: this.name, flag: flag.name } );
+                var result = this.createCreep([WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE], undefined, { class: 'Zealot', home: this.name, flag: flag.name } );
                 if (result == ERR_NOT_ENOUGH_ENERGY) { result = this.createCreep([WORK,CARRY,MOVE], undefined, { class: 'Zealot', home: this.name, flag: flag.name } ); }
                 if (result == OK) { flag.spawned('Zealot'); }
                 return;
