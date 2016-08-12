feac45c0e5f3c52520b379fbe49f312b87993917
Floyd <floyd@atc.no>
2016-08-12 12:44:21 +0200

    added spawn parameters for feeders

--- oop-extend/StructureSpawn.js
+++ oop-extend/StructureSpawn.js
@@ -4,6 +4,12 @@ module.exports = {
 
     initialize: function() {
         //console.log(this+' initializing');
+
+        // Calculate and set spawn parameters for feeder
+        this.memory.lead_time = 10; // How many ticks from spawn to arrival? FIXME!!!
+        this.memory.cooldown = 300; // How many ticks minimum between spawns? FIXME!!!
+        this.memory.workforce = { 'Feeder': 2 };
+
         this.initialize_tc();
     },
 
