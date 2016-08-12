80cd09ee7e467878e4c00545e67210bf3e87f4b5
Floyd <floyd@atc.no>
2016-08-12 15:42:57 +0200

    dedi upgrader where are you

--- oop-extend/Room.js
+++ oop-extend/Room.js
@@ -41,8 +41,10 @@ module.exports = {
         if (this.memory.upgrader != null) {
             var creep = Game.getObjectById(this.memory.upgrader);
             if (creep != null && creep.memory.working == true && creep.memory.task.type == 'upgrade') {
+                console.log(this+' dedicated upgrader is '+creep);
                 this.upgrader = creep;
             } else {
+                console.log(this+' this.memory.upgrader '+this.memory.upgrader+' invalid/missing');
                 this.memory.upgrader = null;
             }
         }
