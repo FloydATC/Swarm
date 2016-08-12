fcf81ba0e0a5864dc9c17c63fe15f9b53a28afc7
Floyd <floyd@atc.no>
2016-08-12 16:16:26 +0200

    this looks better

--- oop-extend/Creep.js
+++ oop-extend/Creep.js
@@ -532,7 +532,7 @@ module.exports = {
             if (this.free > 0) {
                 console.log(this+' has free space');
                 var treasures = this.pos.findInRange(FIND_DROPPED_ENERGY, 1);
-                if (treasures.length > 0) { this.pickup(treasures[0]); this.say('Treasure'); }
+                if (treasures.length > 0) { this.pickup(treasures[0]); }
             }
         } else {
             this.move_to(target);
