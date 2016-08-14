fce3002bb959615db6a3e0c5b6c25b46e44520a3
Floyd <floyd@atc.no>
2016-08-14 22:41:14 +0200

    jhgjh

--- oop-extend/Creep.js
+++ oop-extend/Creep.js
@@ -667,9 +667,11 @@ Creep.prototype.move_to = function(target) {
                     this.memory.useexit = { x: exit.x, y: exit.y, roomName: exit.roomName };
                 }
             }
+            if (this.memory.useexit) {
             target = { pos: new RoomPosition(this.memory.useexit.x, this.memory.useexit.y, this.memory.useexit.roomName) };
             console.log(this+' dummy target = '+JSON.stringify(target));
             console.log(this+' in '+this.room.name+' using exit at '+target.pos);
+            }
         }
     } else {
         delete this.memory.nexthop;
