46191453ab1f1425d89f4a7af7d1f5bb6d55b9c3
Floyd <floyd@atc.no>
2016-08-14 22:00:58 +0200

    ksjghdf

--- oop-extend/Creep.js
+++ oop-extend/Creep.js
@@ -658,6 +658,7 @@ Creep.prototype.move_to = function(target) {
                 console.log(this+' finding closest exit to '+nexthop.room);
                 this.memory.useexit = this.pos.findClosestByPath(this.memory.nexthop.exit);
             }
+            console.log(this+' using exit at '+target.pos);
             target = { pos: this.memory.useexit };
         }
     } else {
