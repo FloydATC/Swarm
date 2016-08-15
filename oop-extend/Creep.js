ef18b3431c0e6b662ccde918ab77fddc5fa257aa
Floyd <floyd@atc.no>
2016-08-15 11:23:01 +0200

    added warning to debug statements that incur massive cpu overhead

--- oop-extend/Creep.js
+++ oop-extend/Creep.js
@@ -615,10 +615,10 @@ Creep.prototype.learn_path = function() {
             y2 = this.memory._move.dest.y;
             var dst = ('00'+x2).slice(-2)+('00'+y2).slice(-2); // XXYY
             //console.log('  learning route src='+src+' dst='+dst+' nexthop='+nexthop);
-            //console.log('BEFORE '+table.asString());
+            //console.log('BEFORE '+table.asString()); // WARNING! PREMATURE CALL TO .asString() = MASSIVE CPU OVERHEAD
             table.setDirectionTo(dst, nexthop);
             //console.log('CHANGE dst='+dst+' nexthop='+nexthop);
-            //console.log('AFTER  '+table.asString());
+            //console.log('AFTER  '+table.asString()); // WARNING! PREMATURE CALL TO .asString() = MASSIVE CPU OVERHEAD
             learned++;
 //            }
         this.room.save_routing_table(src, table);
