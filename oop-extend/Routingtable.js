2f5f0ad78c00dd836cb16330696d4667d028fc41
Andreas Lund <andlun@ansatt.oikt.net>
2016-08-19 10:19:12 +0200

    askdjh

--- oop-extend/Routingtable.js
+++ oop-extend/Routingtable.js
@@ -89,10 +89,11 @@ Routingtable.prototype.setDirectionTo = function(address, direction) {
     if (this.getDirectionTo(address) == direction) { return; } // No change
     if (!_.isString(address)) {
         if (this.binary_expanded == null) { this.expand_binary(); }
-        console.log('PRE= '+this.binary_expanded+' (length='+this.binary_expanded.length+')');
+        console.log('PRE= '+this.binary_expanded.charCodeAt(address));
         console.log(this+' learned address='+address+' direction='+direction);
         this.binary_expanded = this.binary_expanded.substring(0,address)+String.fromCharCode(direction)+this.binary_expanded.substring(address+1);
-        console.log('POST='+this.binary_expanded+' (length='+this.binary_expanded.length+')');
+        console.log('POST='+this.binary_expanded.charCodeAt(address));
+
         return;
     }
     var a = address * 1;
