05504024896650c0ec887b6507e49272089060ac
FloydATC <floyd@atc.no>
2016-08-19 19:40:05 +0200

    debuggg

--- oop-extend/Routingtable.js
+++ oop-extend/Routingtable.js
@@ -100,7 +100,7 @@ Routingtable.prototype.expand_binary = function() {
 
 Routingtable.prototype.compress_binary = function() {
     this.binary_table = '';
-    //this.binary_debug = '';
+    this.binary_debug = '';
     if (this.binary_expanded == null) { return; }
     var span_a1 = null;
     var span_a2 = null;
@@ -119,7 +119,7 @@ Routingtable.prototype.compress_binary = function() {
         }
     }
     this.add_span(span_a1, span_a2, span_dir);
-    //console.log('Compressed '+this.binary_debug);
+    console.log('Compressed '+this.binary_debug);
 }
 
 Routingtable.prototype.add_span = function(addr1, addr2, dir) {
@@ -128,11 +128,11 @@ Routingtable.prototype.add_span = function(addr1, addr2, dir) {
         // Single address
         var code = dir<<12;
         this.binary_table = this.binary_table + String.fromCharCode(addr1 | code);
-        //this.binary_debug = this.binary_debug + addr1+'='+dir+';';
+        this.binary_debug = this.binary_debug + addr1+'='+dir+';';
     } else {
         // Address range
         this.binary_table = this.binary_table + String.fromCharCode(addr1) + String.fromCharCode(addr2 | code);
-        //this.binary_debug = this.binary_debug + addr1+'-'+addr2+'='+dir+';';
+        this.binary_debug = this.binary_debug + addr1+'-'+addr2+'='+dir+';';
     }
 }
 
