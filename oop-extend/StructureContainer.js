0c4acc0ec02ce453d0fe2ccb5232ef9849bd3549
Floyd <floyd@atc.no>
2016-08-19 06:15:48 +0200

    maybe this is why

--- oop-extend/StructureContainer.js
+++ oop-extend/StructureContainer.js
@@ -6,6 +6,7 @@ module.exports = {
         //console.log(this+' initializing');
         this.energy = this.store.energy;
         this.free = this.storeCapacity - _.sum(this.store);
+        this.reserved_amount = 0;
     },
 
 };
