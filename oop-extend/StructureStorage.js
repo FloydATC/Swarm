dc122b4b6cf555208c390a15c72bbb826fc1d9ec
Floyd <floyd@atc.no>
2016-09-02 23:05:11 +0200

    reset storage reserve

--- oop-extend/StructureStorage.js
+++ oop-extend/StructureStorage.js
@@ -6,4 +6,5 @@ StructureStorage.prototype.initialize = function() {
     //console.log(this+' initializing');
     this.energy = this.store.energy;
     this.free = this.storeCapacity - _.sum(this.store);
+    this.reserved_amount = 0;
 }
