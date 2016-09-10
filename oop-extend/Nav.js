f435ef6fbb85789b53f5c87201cfff866465090f
Floyd <floyd@atc.no>
2016-09-10 22:28:49 +0200

    quiesce nav debugging

--- oop-extend/Nav.js
+++ oop-extend/Nav.js
@@ -5,7 +5,7 @@ module.exports = {
     learn_path: function(src, final, path) {
         var debug = false;
         //if (src.roomName != final.roomName) { debug = true; }
-        if (debug) { console.log('NAV> learn_path('+src+', '+final+', '+path+')'); }
+        //if (debug) { console.log('NAV> learn_path('+src+', '+final+', '+path+')'); }
         var from = src;
         var table = null;
         for (var i=0; i<path.length; i++) {
@@ -28,7 +28,7 @@ module.exports = {
                     table.tilename = tilename;
                     table.tablename = tablename;
                 }
-                if (debug) { console.log('NAV>     from '+from+' move '+dir+' to reach '+dest); }
+                //if (debug) { console.log('NAV>     from '+from+' move '+dir+' to reach '+dest); }
                 table.setDirectionTo(dest.x + (dest.y * 50), dir);
                 table.dirty = true;
             }
@@ -42,7 +42,7 @@ module.exports = {
                 table.tilename = tilename;
                 table.tablename = tablename;
             }
-            if (debug) { console.log('NAV>     from '+from+' move '+dir+' to reach '+final+' (final)'); }
+            //if (debug) { console.log('NAV>     from '+from+' move '+dir+' to reach '+final+' (final)'); }
             table.setDirectionTo(final.x + (final.y * 50), dir);
             table.dirty = true;
             module.exports.set_table(table.roomname, table.tilename, table.tablename, table, debug);
@@ -50,7 +50,7 @@ module.exports = {
             from = next;
             if (from.roomName != src.roomName) {
                 // Limit learning to current room
-                if (debug) { console.log('NAV>     will not learn past room boundary'); }
+                //if (debug) { console.log('NAV>     will not learn past room boundary'); }
                 return;
             }
         }
@@ -59,17 +59,17 @@ module.exports = {
     get_direction: function(src, dst) {
         var debug = false;
         //if (src.roomName != dst.roomName) { debug = true; }
-        if (src.roomName == 'E25N38' && dst.roomName != 'E25N38') { debug = true; }
+        //if (src.roomName == 'E25N38' && dst.roomName != 'E25N38') { debug = true; }
         var tilename = ('0'+src.x).slice(-2) + ('0'+src.y).slice(-2); // Format as XXYY
         var tablename = (src.roomName === dst.roomName ? 'local' : dst.roomName);
         var table = module.exports.get_table(src.roomName, tilename, tablename, debug);
         var dir = table.getDirectionTo(dst.x + (50 * dst.y), debug);
-        if (debug) { console.log('NAV>     get_direction('+src+','+dst+') = '+dir); }
+        //if (debug) { console.log('NAV>     get_direction('+src+','+dst+') = '+dir); }
         return dir;
     },
 
     get_table: function(roomname, tilename, tablename, debug) {
-        if (debug) { console.log('NAV>   get_table('+roomname+', '+tilename+', '+tablename+')'); }
+        //if (debug) { console.log('NAV>   get_table('+roomname+', '+tilename+', '+tablename+')'); }
         if (!Memory.rooms[roomname]) { return new Routingtable();  }
         if (!Memory.rooms[roomname].r) { return new Routingtable(); }
         if (!Memory.rooms[roomname].r[tilename]) { return new Routingtable(); }
@@ -79,7 +79,7 @@ module.exports = {
     },
 
     set_table: function(roomname, tilename, tablename, table, debug) {
-        if (debug) { console.log('NAV>   set_table('+roomname+', '+tilename+', '+tablename+', '+table+')'); }
+        //if (debug) { console.log('NAV>   set_table('+roomname+', '+tilename+', '+tablename+', '+table+')'); }
         if (!Memory.rooms[roomname]) { Memory.rooms[roomname] = {}; }
         if (!Memory.rooms[roomname].r) { Memory.rooms[roomname].r = {}; }
         if (!Memory.rooms[roomname].r[tilename]) { Memory.rooms[roomname].r[tilename] = {}; }
