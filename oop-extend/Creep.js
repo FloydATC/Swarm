86b05b277595ce36f2dc3bac2530c64207256066
Floyd <floyd@atc.no>
2016-09-18 19:34:38 +0200

    bugfix

--- oop-extend/Creep.js
+++ oop-extend/Creep.js
@@ -70,7 +70,7 @@ Creep.prototype.execute = function() {
     var target = Game.getObjectById(this.target);
     //console.log(this+' target '+target);
     if (this.memory.class == 'Drone' && target == null) {
-        if (this.room.controller && (this.room.controller.level < 3 || (this.room.energy_reserves() > 25 && this.room.spawn_reserves() > 50))) {
+        if (this.room.controller && (this.room.controller.level < 3 || this.room.spawn_reserves() > 50)) {
             target = this.room.controller;
             this.task = 'upgrade';
             this.target = this.room.controller.id;
